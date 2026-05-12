f35c365b9ae1554f44373d8b8f102f616b1aeb01
brunardo19 <78654852+brunardo19@users.noreply.github.com>
2026-05-12 18:30:12 -0300

    Version to 1.0.1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Guilty Gear 2 Overture Loadlist importer",
     "author": "Brunardo",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (4, 5, 0),
     "location": "File > Import",
     "description": "Uses the loadlist.bin to import full characters with meshes, textures and animations",
